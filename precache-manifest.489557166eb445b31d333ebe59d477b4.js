07dce583664f10636ef55ca6eef01512a062b2d5
B <b@Bs-iMac.hsd1.ca.comcast.net>
2020-10-20 16:31:38 -0700

    Updates

---
+++ precache-manifest.489557166eb445b31d333ebe59d477b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfaa7ee151f0a9cf31b25c64d6b6423d",
+    "url": "/react-2/index.html"
+  },
+  {
+    "revision": "8ff1d2fac832a2477f14",
+    "url": "/react-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b758a537a5bc106981f",
+    "url": "/react-2/static/js/2.769a8f58.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-2/static/js/2.769a8f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff1d2fac832a2477f14",
+    "url": "/react-2/static/js/main.3b31a320.chunk.js"
+  },
+  {
+    "revision": "5e721ee5e9e035096495",
+    "url": "/react-2/static/js/runtime-main.81d6a988.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
